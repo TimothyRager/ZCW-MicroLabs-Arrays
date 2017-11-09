287126f5f9631615b941a6c460dd1f630ecbfd6c
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-11-09 17:00:44 -0500

    ArrayParty lab

--- src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
+++ src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
@@ -1,26 +1,127 @@
 package io.zipcoder.microlabs.arrays;
 
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+//import java.util.stream.IntStream;
+import java.util.stream.*;
 
 public class ArrayParty {
 
-    public String printArray(String[] inputArray){
-        return "";
+    public String printArray(String[] inputArray) {
+        String returnMe = "*** Output ***";
+        for (String s : inputArray) {
+            returnMe += "\n" + s;
+        }
+
+        return returnMe;
+
     }
 
-    public String lastElement(String[] inputArray){
-        return "";
+    public String lastElement(String[] inputArray) {
+        return "*** Output ***\n" + inputArray[inputArray.length - 1];
     }
 
-    //TODO Define the method lastButOne
+    public String lastButOne(String[] inputArray) {
+        return "*** Output ***\n" + inputArray[inputArray.length - 2];
+    }
 
-    //TODO Define the method reverse
 
-    //TODO Define the method isPalindrome
+    public String[] reverse(String[] inputArray) {
 
-    //TODO Define the method compress
+        String[] intermediate = new String[inputArray.length];
+        int j = 0;
+        for (int i = inputArray.length - 1; i >= 0; i--) {
+            intermediate[j] = inputArray[i];
+            j++;
+        }
 
-    //TODO Define the method pack
+        return (intermediate);
 
+    }
+
+    public boolean isPalindrome(String[] inputArray){
+
+    int j=inputArray.length-1;
+    for (int i=0; i<=j; i++){
+        if (!inputArray[i].equals(inputArray[j]))
+        {
+            return false;
+        }
+        j--;
+    }
+    return true;
+
+}
+
+    public int[] compress(int[] inputArray){
+
+        int nonRepeat=1;
+
+        for (int i=1; i<inputArray.length; i++)
+        {
+            if (inputArray[i]!=inputArray[i-1])
+            {
+                nonRepeat++;
+            }
+        }
+
+        int[] returnMe = new int[nonRepeat];
+
+        nonRepeat=1;
+        returnMe[0]=inputArray[0];
+        for (int i=1; i<inputArray.length; i++)
+        {
+            if (inputArray[i]!=inputArray[i-1])
+            {
+                returnMe[nonRepeat]=inputArray[i];
+                nonRepeat++;
+            }
+        }
+
+
+        return (returnMe);
+    }
+
+    public String[] pack(char[] inputArray){
+
+        char toPack=inputArray[0];
+        int neededReturnIndexes=1;
+        for (int i=1; i<inputArray.length; i++)
+        {
+            if (inputArray[i]!=toPack)
+            {
+                neededReturnIndexes++;
+                toPack=inputArray[i];
+            }
+        }
+
+        String[] returnMe = new String[neededReturnIndexes];
+
+        for (int i=0; i<returnMe.length; i++){
+            returnMe[i]="";
+        }
+
+        toPack=inputArray[0];
+        int rmIterator=0;
+        returnMe[rmIterator]+=toPack;
+        for (int i=1; i<inputArray.length; i++)
+        {
+            if (inputArray[i]==toPack)
+            {
+                returnMe[rmIterator]+=toPack;
+                continue;
+            }
+            toPack=inputArray[i];
+            rmIterator++;
+            returnMe[rmIterator]+=toPack;
+        }
+
+
+        return (returnMe);
+    }
 
 
 }
+
--- src/test/java/io/zipcoder/microlabs/arrays/ArrayPartyTest.java
+++ src/test/java/io/zipcoder/microlabs/arrays/ArrayPartyTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class ArrayPartyTest {
 
     @Test
-    public void printArrayTest(){
+    public void printArrayTest() {
         //: Given
         String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
         ArrayParty arrayParty = new ArrayParty();
@@ -26,23 +26,111 @@ public class ArrayPartyTest {
     }
 
     @Test
-    public void lastElementTest(){
-        //: Given
+    public void lastElementTest() {
 
-        //: When
+        ArrayParty arrayParty = new ArrayParty();
 
-        //: Then
+        String[] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\nMushrooms";
+        String actual = arrayParty.lastElement(breakfast);
+
+        Assert.assertEquals(expected, actual);
+
     }
 
-    //TODO Define the method lastButOneTest
+    @Test
+    public void lastButOneTest() {
+        //: Given
+        ArrayParty arrayParty = new ArrayParty();
+        String[] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\nTomatoes";
+        String actual = arrayParty.lastButOne(breakfast);
 
-    //TODO Define the method reverseTest
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void reverseTest() {
+        //: Given
+        ArrayParty arrayParty = new ArrayParty();
+        String[] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\nMushrooms\nTomatoes\n" +
+                "Bacon\nBeans\nEggs\nSausage";
+        String[] actualArray = arrayParty.reverse(breakfast);
+        String actual = "*** Output ***";
+
+        for (String s : actualArray) {
+            actual += "\n" + s;
+        }
+
+        Assert.assertEquals(expected, actual);
+    }
 
     //TODO Define the method isPalindromeTest
+    @Test
+    public void isPalindromeTest(){
 
-    //TODO Define the method compressTest
+        ArrayParty arrayParty = new ArrayParty();
+
+        String[] palindromic = {"Sausage", "Eggs", "Beans",
+            "Beans", "Eggs", "Sausage"};
+        String[] breakfast = {"Sausage", "Eggs", "Beans",
+            "Bacon", "Tomatoes", "Mushrooms"};
+
+        String expected = "*** Output ***\ntrue\nfalse";
+        String actual = "*** Output ***";
+
+        actual+="\n"+arrayParty.isPalindrome(palindromic)
+               +"\n"+arrayParty.isPalindrome(breakfast);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void compressTest(){
+
+        ArrayParty arrayParty = new ArrayParty();
+
+        int[] nums = {1,1,3,3,3,2,2,2,1,1,1,1,4,4,4,4};
+        String expected = "*** Output ***\n1\n3\n2\n1\n4";
+        int[] actualArray = arrayParty.compress(nums);
+        String actual = "*** Output ***";
+        for (Integer i : actualArray){
+            actual+="\n"+i;
+        }
+
+        Assert.assertEquals(expected, actual);
+    }
 
     //TODO Define the method packTest
+    @Test
+    public void packTest(){
+
+        ArrayParty arrayParty = new ArrayParty();
+
+        char[] letters = {'a', 'a', 'a', 'a',
+                          'b',
+                          'c', 'c',
+                          'a', 'a',
+                          'd',
+                          'e', 'e', 'e', 'e'};
+        String expected = "*** Output ***\naaaa, b, cc, aa, d, eeee";
+        String[] actualArray = arrayParty.pack(letters);
+        String actual = "*** Output ***\n";
+
+        for (String s : actualArray){
+            actual+=s+", ";
+        }
+        actual = actual.substring(0,actual.length()-2);
+
+        Assert.assertEquals(expected, actual);
+
+
+    }
 
 
 }
